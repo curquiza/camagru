5b8eb03874d5c8663446f967bc18971c1ac6f057
Clurquiz <clementine.urquizar@gmail.com>
2018-03-13 00:02:56 +0100

    Upd : create table class

--- config/setup.php
+++ config/setup.php
@@ -3,8 +3,32 @@
 require_once 'app/connection.php';
 // require_once 'config/database.php';
 
+class SQLiteCreateTable {
+
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function createTables() {
+        $commands = ['CREATE TABLE IF NOT EXISTS users (
+                        user_id      INTEGER PRIMARY KEY,
+                        pseudo       TEXT NOT NULL
+                      )'];
+        foreach ($commands as $command) {
+            $this->pdo->exec($command);
+        }
+    }
+
+
+}
+
+
 $pdo = (new SQLiteConnection())->connect();
 if ($pdo != null)
   echo "yes !";
 else
   echo "raté";
+$sqlite = new SQLiteCreateTable($pdo);
+$sqlite->createTables();
