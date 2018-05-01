1c6f81717cc227e3e35e2073012a1d6683dd2ad5
Clurquiz <clementine.urquizar@gmail.com>
2018-05-01 15:57:39 +0200

    add : protected params and construct in comment model

--- app/models/comment.php
+++ app/models/comment.php
@@ -4,4 +4,12 @@ require_once 'base.php';
 
 class Comment extends Base {
 
+    protected $id;
+    protected $content;
+    protected $created_at;
+    protected $user_id;
+    protected $photo_id;
+
+    public function __construct($id) {
+    }
 }
