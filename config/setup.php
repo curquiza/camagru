da452f6103d42246facc715745d3c5c46ed641fc
Clurquiz <clementine.urquizar@gmail.com>
2018-05-10 15:49:35 +0200

    add init file for requires

--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'app/models/connection.php';
+require_once __DIR__ . '/app/models/connection.php';
 
 class SQLiteCreateTable {
 
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once 'config/router.php';
+require_once __DIR__ . '/config/init.php';
 
 Router::go_to($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
