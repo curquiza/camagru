27611a29b72fbfd638d2b0f98b76a1839455d9b8
Clurquiz <clementine.urquizar@gmail.com>
2018-05-10 22:17:57 +0200

    add layout

---
+++ app/views/layout/application.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="../../../css/spectre.css">
+	<link rel="stylesheet" href="../../../css/spectre-exp.css">
+	<link rel="stylesheet" href="../../../css/spectre-icons.css">
+</head>
+
+<body>
+	<h1> HEADER </h1>
+    <?php include('app/views/users/show.php'); ?>
+</body>
+
+<footer> FOOTER </footer>
+</html>
