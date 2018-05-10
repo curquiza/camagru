a6cb9e0eeb7a042697fcf6bafca4315be403d73a
Clurquiz <clementine.urquizar@gmail.com>
2018-05-10 22:18:16 +0200

    add base controller

---
+++ app/controllers/base_controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class BaseController {
+
+    public function load_layout() {
+        // $view = new View('layout', 'application');
+        // $view->assign('user_id', $_GET['user_id']);
+    }
+}
